98b94eebb527494dfb3e0f17506ceacc15808323
zhyldyz1 <zh.toktorova@gmail.com>
2025-05-15 16:34:24 -0500

    session4

---
+++ session4/userdata.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo dnf update -y
+sudo dnf install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+cat <<EOF > /var/www/html/index.html
+<html>
+  <body>
+    <h1> ${environment} instance is running!</h1>
+  </body>
+</html>
+EOF
+
+
+
+
+# Environment =  var.env
+# $public_ip = 
